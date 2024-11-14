0fb14c5e9efcbad51fb8c42461ea070114b57efb
bluewhale36 <kmsuhyn36@gmail.com>
2024-11-14 14:55:27 +0900

    2024/11/14 우승훈 / Security 권한 처리 로직 수정 완료
    ## Back End
    
    ### GrantedAuthority
    
    1. GrantedAuthority_Impl.java 파일 생성.
    2. GrantedAuthority Interface Implementation.
    
    ### 이 외
    
    1. Role.java 의 필드 변수 roleName 추가.
        1. ROLE_ 접두사 포함된 문자열 저장.
    2. Role.java 의 getGrantedAuthority() method 수정.
        1. GrantedAuthority_Impl 객체 반환.

--- src/main/java/com/guncat/ecommerce/users/enums/Role.java
+++ src/main/java/com/guncat/ecommerce/users/enums/Role.java
@@ -1,5 +1,6 @@
 package com.guncat.ecommerce.users.enums;
 
+import com.guncat.ecommerce.security.util.GrantedAuthority_Impl;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 
@@ -8,13 +9,15 @@ import org.springframework.security.core.GrantedAuthority;
  */
 @Getter
 public enum Role {
-    CEO("사장"),
-    STAFF("직원"),
-    REGULAR("일반 회원");
+    CEO("ROLE_CEO", "사장"),
+    STAFF("ROLE_STAFF", "직원"),
+    REGULAR("ROLE_REGULAR", "일반 회원");
 
+    private final String roleName;
     private final String description;
 
-    Role(String description) {
+    Role(String roleName, String description) {
+        this.roleName = roleName;
         this.description = description;
     }
 
@@ -22,9 +25,9 @@ public enum Role {
      * 사용자 권한을 {@link GrantedAuthority} 구현체로 반환하는 method.<br/>
      * Spring Security 에서 개발자 정의의 권한을 인식하도록 하기 위함.
      *
-     * @return {@link GrantedAuthority} 구현체.
+     * @return {@link GrantedAuthority_Impl}
      */
     public GrantedAuthority getGrantedAuthority() {
-        return this::name;
+        return new GrantedAuthority_Impl(this);
     }
 }
