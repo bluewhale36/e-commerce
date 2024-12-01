2ae60d9a30852dae4ecf49660dd30cebc1c7aa70
bluewhale36 <kmsuhyn36@gmail.com>
2024-12-01 16:14:10 +0900

    2024/12/01 우승훈 / 관리자 권한 회원 조회 기능 구현 진행 중
    ## Back End
    
    ### Admin
    
    1. Admin Package 내 요구사항 도메인 별 하위 package 설계.
    
    ### Admin Users
    
    1. admin/users package 내 AdminUsersController.java 추가.
    
    ### Users Service
    
    1. 관리자 권한의 회원 조회 기능을 위한 Service Layer 내 method 추가.
        1. `getAllUsersCount() :Long`
        2. `getUsersByPageNum(pageNum :int) :PagingResponseDTO<UsersDTO>`
        3. `getUserByUserCode(userCode :String) :UsersDTO`
    
    ### 이 외
    
    1. common/dto package 생성.
    2. common/dto package 내 페이징 결과 데이터 교환 목적의 PagingResponseDTO.java 추가.
    3. UsersMapper 내 권한 기준의 정렬 로직 추가.
    4. AdminHomeController 구현. 관리자 기본 페이지 경로 mapping 용도.
    
    ## Front End
    
    ### SCSS
    
    1. 텍스트 스타일의 클래스 정의를 위한 text-style.scss 파일 생성 및 구현 완료.
    
    ### CSS
    
    1. screen directory 내 text-style.scss compiled 파일 생성됨. (text-style.css)
    2. admin directory 내 users 하위 directory 설계 및 users.css 파일 생성.
    
    ### JS
    
    1. admin directory 내 users.js 파일 생성. 추후 하위 directory 세분화 예정.
    2. admin/users.js 내 페이징 기능 구현.
    
    ### HTML
    
    1. admin directory 내 home, users, ceo 하위 directory 설계.
    2. admin/home directory 내 home.html 구현. 관리자 기본 페이지 렌더링 용도.
    3. admin/users directory 내 users.html 및 user-details.html 구현.
        1. users.html : 관리자 권한 회원 조회 페이지.
        2. user-details.html : 관리자 권한 회원 정보 상세 조회 페이지.
    4. fragments/container.html 내 text-style.css 파일에 대한 link 태그 추가.

--- src/main/resources/static/js/admin/users.js
+++ src/main/resources/static/js/admin/users.js
@@ -9,3 +9,8 @@ function getPreviousPage(e) {
     $(e).attr('disabled', true);
     location.href=`/admin/users?page=${currentPageNum - 1}`;
 }
+
+function goTo(e) {
+    $(e).attr('disabled', true);
+    location.href=`/admin/users?page=${$(e).text() - 1}`;
+}
